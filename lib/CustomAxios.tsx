5eb843659eabdebdd235d8c51086be5a68624563
younghyunHan <atuniverse@naver.com>
2022-12-06 15:34:51 +0900

    Fix: axios 모듈 컴포넌트 수정

--- lib/CustomAxios.tsx
+++ lib/CustomAxios.tsx
@@ -6,5 +6,6 @@ export const customAxios: AxiosInstance = axios.create({
   baseURL: "http://localhost:3000", // 기본 서버 주소 입력
   headers: {
     Authorization: `${access_token}`,
+    "Content-Type": "multipart/form-data",
   },
 });
