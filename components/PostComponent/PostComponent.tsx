30a18e1c61e7e55b338ac0d7e9b675b678d14c50
younghyunHan <atuniverse@naver.com>
2022-12-12 09:47:47 +0900

    Fix: post 컴포넌트 코드 PostComponet코드로 이전 - useRef를 활용해 TOAST UI Editor 내용을 가져오는 문제가 있었음 - Next.js로 인해 Dynamic Import 이슈가 있어 따로 Editor컴포넌트를 분리해서 생긴 문제였음 - 해결하기 위해 Props를 활용해 함수를 내리고, Recoil등을 사용하는 방법을 시도함. - 해결방법은 전체코드를 PostComponent로 내려 버튼이 눌려질 때 useRef를 가져올 수 있도록 하는 것.

---
+++ components/PostComponent/PostComponent.tsx
@@ -0,0 +1,131 @@
+import { useEffect, useState, useRef, useCallback } from "react";
+import { Editor } from "@toast-ui/react-editor";
+
+import axios from "axios";
+import { customAxios } from "../../lib/CustomAxios";
+
+import PostComponentStyles from "./PostComponent.module.css";
+
+// TOAST UI Editor CSS
+import "@toast-ui/editor/dist/toastui-editor.css";
+
+// color-syntax CSS
+import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
+import "tui-color-picker/dist/tui-color-picker.css";
+import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
+
+// code syntax highlight
+import "prismjs/themes/prism.css";
+import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
+import Prism from "prismjs"; // prism 테마 추가
+import "prismjs/components/prism-clojure.js";
+import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight";
+
+// table-merged-cell
+import tableMergedCell from "@toast-ui/editor-plugin-table-merged-cell";
+
+const PostComponent = () => {
+  const postTitleRef = useRef<HTMLInputElement>(null);
+  const postThumnailLinkRef = useRef<HTMLInputElement>(null);
+  const postThumbnailRef = useRef<HTMLInputElement>(null);
+  const editorRef = useRef<any>(null);
+
+  const editorInstance = editorRef?.current?.getInstance();
+  const postMainHtml = editorInstance?.getHTML();
+
+  const access_token = localStorage.getItem("token");
+
+  const postUploadButtonClick = useCallback(
+    (event: React.MouseEvent<HTMLElement>) => {
+      event.preventDefault();
+      if (!postThumbnailRef.current) {
+        return;
+      }
+
+      const savedPostTitle = postTitleRef?.current?.value as string;
+      const savedThumbnailLink = postThumnailLinkRef?.current?.value as string;
+      const savedThumbnailImgs = postThumbnailRef.current.files as FileList;
+
+      const formData = new FormData();
+
+      // console.log(savedPostTitle);
+      // console.log(savedThumbnailImgs[0]);
+      // console.log(savedThumbnailLink);
+      // console.log(postMainText);
+
+      formData.append("postTitle", savedPostTitle);
+      formData.append("postThumbnailLink", savedThumbnailLink);
+      formData.append("postThumnailImg", savedThumbnailImgs[0]);
+      formData.append("postMaintext", postMainHtml);
+
+      // axios
+      //   .post("http://localhost:3000/userInfoUpdate", formData, {
+      //     headers: {
+      //       // Authorization: `${access_token}`,
+      //       "Content-Type": "multipart/form-data",
+      //     },
+      //   })
+      //   .then(function (response) {
+      //     if (response.data.message === "SUCCESS") {
+      //       alert("게시글 저장되었습니다.");
+      //     }
+      //   });
+
+      // customAxios.post("/userInfoUpdate", formData); // customAxios
+    },
+    [postTitleRef, postThumnailLinkRef, postThumbnailRef, postMainHtml]
+  );
+
+  return (
+    <section id={PostComponentStyles.post}>
+      <h1 id={PostComponentStyles.postPageTitle}>ADD POST</h1>
+      <form id={PostComponentStyles.postPageForm}>
+        <h2 id={PostComponentStyles.postTitle}>Title</h2>
+        <input
+          id={PostComponentStyles.postTitleInput}
+          type="text"
+          name="postTitle"
+          placeholder="제목을 입력해주세요."
+          ref={postTitleRef}
+        />
+        <h2 id={PostComponentStyles.postThumbnail}>Thumbnail</h2>
+        <div id={PostComponentStyles.postThumnailWrap}>
+          <input
+            id={PostComponentStyles.postThumnailLink}
+            type="text"
+            name="postThumbnailLink"
+            placeholder="링크를 입력해주세요."
+            ref={postThumnailLinkRef}
+          />
+          <span>or</span>
+          <input
+            id={PostComponentStyles.postThumnailInput}
+            type="file"
+            accept="image/*"
+            ref={postThumbnailRef}
+          />
+        </div>
+        <Editor
+          ref={editorRef}
+          initialValue="hello react editor world!"
+          previewStyle="vertical"
+          height="500px"
+          usageStatistics={false}
+          plugins={[
+            colorSyntax,
+            tableMergedCell,
+            [codeSyntaxHighlight, { highlighter: Prism }],
+          ]}
+        />
+        <button
+          id={PostComponentStyles.postUploadBtn}
+          onClick={postUploadButtonClick}
+        >
+          게시글 작성
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default PostComponent;
--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,112 +1,26 @@
 import React, { useRef, useCallback, useState } from "react";
-import { useRecoilValue } from "recoil";
-import { postMainTextState } from "../../states";
-import axios from "axios";
 import dynamic from "next/dynamic";
 
 // components
 import Nav from "../../components/Nav/Nav";
 import HeaderTitle from "../../components/HeaderTitle/HeaderTitle";
-import { customAxios } from "../../lib/CustomAxios";
 
 // CSS
 import PostStyles from "./post.module.css";
 
 export default function Post() {
-  // const access_token = localStorage.getItem('token');
-  const [postMainText, setPostMainText] = useState("");
-
   const Editor = dynamic(
-    () => import("../../components/Editor/EditorComponent"),
+    () => import("../../components/PostComponent/PostComponent"),
     {
       ssr: false,
     }
-  );
-
-  const postTitleRef = useRef<HTMLInputElement>(null);
-  const postThumnailLinkRef = useRef<HTMLInputElement>(null);
-  const postThumbnailRef = useRef<HTMLInputElement>(null);
-  // const postMainText = useRecoilValue(postMainTextState);
-
-  const postUploadButtonClick = useCallback(
-    (event: React.MouseEvent<HTMLElement>) => {
-      event.preventDefault();
-      if (!postThumbnailRef.current) {
-        return;
-      }
-
-      const savedPostTitle = postTitleRef?.current?.value as string;
-      const savedThumbnailLink = postThumnailLinkRef?.current?.value as string;
-      const savedThumbnailImgs = postThumbnailRef.current.files as FileList;
-
-      const formData = new FormData();
-
-      console.log(savedPostTitle);
-      console.log(savedThumbnailImgs[0]);
-      console.log(savedThumbnailLink);
-      console.log(postMainText);
-
-      formData.append("postTitle", savedPostTitle);
-      formData.append("postThumbnailLink", savedThumbnailLink);
-      formData.append("postThumnailImg", savedThumbnailImgs[0]);
-      formData.append("postMaintext", postMainText);
-
-      // axios
-      //   .post("http://localhost:3000/userInfoUpdate", formData, {
-      //     headers: {
-      //       // Authorization: `${access_token}`,
-      //       "Content-Type": "multipart/form-data",
-      //     },
-      //   })
-      //   .then(function (response) {
-      //     if (response.data.message === "SUCCESS") {
-      //       alert("게시글 저장되었습니다.");
-      //     }
-      //   });
-
-      // customAxios.post("/userInfoUpdate", formData); // customAxios
-    },
-    [postTitleRef, postThumnailLinkRef, postThumbnailRef, postMainText]
   );
 
   return (
     <>
       <HeaderTitle />
       <Nav />
-      <section id={PostStyles.post}>
-        <h1 id={PostStyles.postPageTitle}>ADD POST</h1>
-        <form id={PostStyles.postPageForm}>
-          <h2 id={PostStyles.postTitle}>Title</h2>
-          <input
-            id={PostStyles.postTitleInput}
-            type="text"
-            name="postTitle"
-            placeholder="제목을 입력해주세요."
-            ref={postTitleRef}
-          />
-          <h2 id={PostStyles.postThumbnail}>Thumbnail</h2>
-          <div id={PostStyles.postThumnailWrap}>
-            <input
-              id={PostStyles.postThumnailLink}
-              type="text"
-              name="postThumbnailLink"
-              placeholder="링크를 입력해주세요."
-              ref={postThumnailLinkRef}
-            />
-            <span>or</span>
-            <input
-              id={PostStyles.postThumnailInput}
-              type="file"
-              accept="image/*"
-              ref={postThumbnailRef}
-            />
-          </div>
-          <Editor />
-          <button id={PostStyles.postUploadBtn} onClick={postUploadButtonClick}>
-            게시글 작성
-          </button>
-        </form>
-      </section>
+      <Editor />
     </>
   );
 }
